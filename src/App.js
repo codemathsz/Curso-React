0589a2c75ef916da6ced99f48ad1d8afe2a8d156
Matheus <mo284891@gmail.com>
2022-07-26 21:55:22 -0300

    Trabalhando com props - Aula 05

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';// importando componente 
+import Pessoa from './components/Pessoa';
+import SayMyName from './components/SayMyName';
 
 function App() {
 
+  const nome = "Maria"
+
   return (
     <div className="App">
-    
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="Kaique"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+        nome="Rodrigo" 
+        idade="35" 
+        profissao="Programador" 
+        foto="https://via.placeholder.com/150"
+        />
     </div>
   )
 }
